3b533d1093d9c42cc53f531d71c56c76cbdfc5ac
Danang Ponorogo <danang.dandies@gmail.com>
2023-05-26 17:01:44 +0700

    add second db

--- app/Models/BezzetingModel.php
+++ app/Models/BezzetingModel.php
@@ -6,6 +6,7 @@ use CodeIgniter\Model;
 
 class BezzetingModel extends Model
 {
+    protected $DBGroup          = 'bezzeting';
     protected $table            = 'skpd_bezzeting';
     protected $primaryKey       = 'id';
 }
